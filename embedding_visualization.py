b2751f67ee22462a65443203a032693abf89421c
Alex Meyerowitz <alexandermeyerowitz@gmail.com>
2023-05-10 21:09:21 -0400

    Update embedding_visualization.py (#7)

--- embedding_visualization.py
+++ embedding_visualization.py
@@ -1,10 +1,13 @@
 from comet_ml import Experiment
 import torch
 import typer
+import re
+import numpy as np
 from typing import Optional
 from tqdm import tqdm
 from models import DebertaBase, BertBase
 from preprocess import load_knn_data
+from transformers import AutoTokenizer
 
 device = 'cuda' if torch.cuda.is_available() else "cpu"
 
@@ -41,9 +44,52 @@ def visualize_embeddings(experiment, model, model_name, batch_size):
 
         experiment.log_embedding(test_embeds, test_labels, title='Test embeddings')
 
+def visualize_embedding_with_article_labels(experiment, model, model_name):
+    with experiment.test():
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        rgx = re.compile('(\\[.*?\\])')
+
+        # Generate embeddings for knn training set.
+        train_loader, test_loader = load_knn_data(model_name, batch_size=1)
+        train_embeds = []
+        train_labels = []
+        with torch.no_grad():
+            for articles, targets in tqdm(train_loader):
+                articles = articles.to(device)
+                decoded_article_chunk = tokenizer.decode(articles[0])
+                decoded_article_chunk = re.sub(rgx, '', decoded_article_chunk)
+
+                train_embeds.append(model(articles))
+                train_labels.append(f"{'Fake' if targets[0] == 0 else 'Real'} article: {decoded_article_chunk}")
+
+        # Flatten lists
+        train_embeds = torch.cat(train_embeds).detach().cpu().numpy()
+        train_labels = np.array(train_labels)
+
+        experiment.log_embedding(train_embeds, train_labels, title='Train article label embeddings')
+
+        # Generate embeddings for knn test set.
+        test_embeds = []
+        test_labels = []
+        with torch.no_grad():
+            for articles, targets in tqdm(test_loader):
+                articles = articles.to(device)
+                decoded_article_chunk = tokenizer.decode(articles[0])
+                decoded_article_chunk = re.sub(rgx, '', decoded_article_chunk)
+
+                test_embeds.append(model(articles))
+                test_labels.append(f"{'Fake' if targets[0] == 0 else 'Real'} article: {decoded_article_chunk}")
+
+        # Flatten lists
+        test_embeds = torch.cat(test_embeds).detach().cpu().numpy()
+        test_labels = np.array(test_labels)
+
+        experiment.log_embedding(test_embeds, test_labels, title='Test article label embeddings')
+
+
 def main(
     model_name: Optional[str] = typer.Option('microsoft/deberta-base', help='The model name for the model_file.'),
-    model_file: Optional[str] = typer.Option('model.pt', help='File name for the trained embedding model.'),
+    model_file: Optional[str] = typer.Option('models/triplet_nf_1234_4e_10a.pt', help='File name for the trained embedding model.'),
     batch_size: Optional[int] = typer.Option(16, help='The batch size for data processing in the embedding and knn models.')): 
 
     experiment = Experiment(
@@ -59,7 +105,8 @@ def main(
         model = BertBase(model_name, 768, freeze=True).to(device)
     model.load_state_dict(torch.load(model_file, map_location=device))
 
+    visualize_embedding_with_article_labels(experiment, model, model_name)
     visualize_embeddings(experiment, model, model_name, batch_size)
 
 if __name__=="__main__":
-    main('microsoft/deberta-base', 'models/contrast_deberta_all.pt', 16)
+    main('microsoft/deberta-base', 'models/triplet_nf_1234_4e_10a.pt', 16)
